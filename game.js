0ce7be94904c8d1a794f15694ff75c04f7d821e2
Evelyn Keeley <evelynk18@vt.edu>
2022-04-13 15:45:03 -0400

    putting methods inside each other

--- game.js
+++ game.js
@@ -35,68 +35,68 @@ let questions = [
         answer: "the sport of falconry has no impact on the wild raptor populations in the US.",
     },
     {
-        question: '',
-        choice1: "",
-        choice2: "",
-        choice3: "",
-        choice4: "",
-        answer: "",
+        question: 'A potential sponsor requires the following before he or she will agree to shepherd an apprentice into the sport:',
+        choice1: "payment for his/her tutelage from the apprentice.",
+        choice2: "specific evidence of a sincere commitment on the part of the potential apprentice to undertake all aspects of learning about the sport.",
+        choice3: "a note from the potential apprentice’s spouse or parent that it’s okay with him/her if the aspirant undertakes the sport.",
+        choice4: "attendance at all VFA events, and help with cleaning the sponsor’s raptor facilities.",
+        answer: "specific evidence of a sincere commitment on the part of the potential apprentice to undertake all aspects of learning about the sport.",
     },
     {
-        question: '',
-        choice1: "",
-        choice2: "",
-        choice3: "",
-        choice4: "",
-        answer: "",
+        question: 'Not all land is suited to flying a bird of prey, no matter how much prey might be found there. Which of the following would make an ethical, safe falconer consider NOT flying a bird?',
+        choice1: "Property where the best prey habitat runs along a busy road.",
+        choice2: "An enormous tract of land with plenty of habitat, but where the falconer does not have permission.",
+        choice3: "A semi-suburban area or industrial park property where a cat is visible, prowling the brush.",
+        choice4: "All of the above.",
+        answer: "All of the above.",
     },
     {
-        question: '',
-        choice1: "",
-        choice2: "",
+        question: 'It’s perfectly all right to use raptors permitted under a falconry license for personal gain. Many falconers allow their birds to make paid appearances in films and commer- cials to help support their falconry.',
+        choice1: "True",
+        choice2: "False",
         choice3: "",
         choice4: "",
-        answer: "",
+        answer: "False",
     },
     {
-        question: '',
-        choice1: "",
-        choice2: "",
-        choice3: "",
-        choice4: "",
-        answer: "",
+        question: 'The two most important characteristics of a personality capable of becoming a good falconer are',
+        choice1: "curiosity and a mindset that includes lifelong learning.",
+        choice2: "stubbornness and a tendency to judge others.",
+        choice3: "patience and persistence.",
+        choice4: "a and c only.",
+        answer: "a and c only.",
     },
     {
-        question: '',
-        choice1: "",
-        choice2: "",
-        choice3: "",
-        choice4: "",
-        answer: "",
+        question: 'The definition of falconry is',
+        choice1: "the pursuit of wild game in its natural habitat by means of a trained bird of prey.",
+        choice2: "holding a bird on the glove and attending renaissance fairs.",
+        choice3: "using a better-than-average knowledge of raptor biology to rehabilitate birds of prey so they can return to the wild.",
+        choice4: "doing educational presentations surrounding birds of prey, their habitats, conservation, biology, and natural history for groups from scouts to bird watchers.",
+        answer: "the pursuit of wild game in its natural habitat by means of a trained bird of prey.",
     },
     {
-        question: '',
-        choice1: "",
-        choice2: "",
+        question: 'The methods used for training a bird of prey are just like those used in training a hunting dog or a hunter/jumper horse.',
+        choice1: "True",
+        choice2: "False",
         choice3: "",
         choice4: "",
-        answer: "",
+        answer: "False",
     },
     {
-        question: '',
-        choice1: "",
-        choice2: "",
-        choice3: "",
-        choice4: "",
-        answer: "",
+        question: 'Which of the following is the best approach to the various pieces and styles/types of equipment used in falconry?',
+        choice1: "Make everything you will use in falconry, from facilities to gauntlets; from hoods to swivels.",
+        choice2: "Caring for and maintaining the equipment you cannot make yourself.",
+        choice3: "Annually replacing any equipment that is attached to the bird for safety’s sake.",
+        choice4: "b and c above.",
+        answer: "b and c above.",
     },
     {
-        question: '',
-        choice1: "",
-        choice2: "",
-        choice3: "",
-        choice4: "",
-        answer: "",
+        question: 'Falconry permits in the United States are regulated, administered, and enforced by each state or territory’s game department. All of these agencies have different names, and to find your state’s department, search for the group that manages hunting licens- es. In the Commonwealth of Virginia, our regulating agency is which of the following?',
+        choice1: "The federal Fish and Wildlife Service.",
+        choice2: "The Virginia Army Reserves.",
+        choice3: "Officials at the university nearest where you live.",
+        choice4: "The Department of Wildlife Resources.",
+        answer: "The Department of Wildlife Resources.",
     },
     {
         question: '',
@@ -145,27 +145,29 @@ getNewQuestion = () => {
     availableQuestions.splice(questionIndex, 1)
     acceptingAnswers = true
     console.log("HERE")
+
+    choices.forEach(choice => {
+        choice.addEventListener('click', (e) => {
+            if(!acceptingAnswers) return
+            acceptingAnswers = false
+    
+            const selectedChoice = e.currentTarget
+            const selectedAnswer = selectedChoice.dataset['number']
+            let classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect'
+    
+            if(classToApply === 'correct') {
+                incrementScore(SCORE_POINTS)
+            }
+            selectedChoice.parentElement.classList.add(classToApply)
+            setTimeout(() => {
+                selectedChoice.parentElement.classList.remove(classToApply)
+                getNewQuestion()
+            }, 1000)
+        })
+    })
 }
 
-choices.forEach(choice => {
-    choice.addEventListener('click', (e) => {
-        if(!acceptingAnswers) return
-        acceptingAnswers = false
 
-        const selectedChoice = e.currentTarget
-        const selectedAnswer = selectedChoice.dataset['number']
-        let classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect'
-
-        if(classToApply === 'correct') {
-            incrementScore(SCORE_POINTS)
-        }
-        selectedChoice.parentElement.classList.add(classToApply)
-        setTimeout(() => {
-            selectedChoice.parentElement.classList.remove(classToApply)
-            getNewQuestion()
-        }, 1000)
-    })
-})
 
 incrementScore = num => {
     score += num
