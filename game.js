7fdc4d21532372a55d827ac4bd4e809aa3c6858c
Evelyn Keeley <evelynk18@vt.edu>
2022-04-13 16:08:56 -0400

    Added number back in

--- game.js
+++ game.js
@@ -179,6 +179,7 @@ getNewQuestion = () => {
     question.innerText = currentQuestion.question
 
     choices.forEach(choice =>  {
+        const number = choice.dataset['number']
         choice.innerText = currentQuestion['choice' + number]
     })
 
